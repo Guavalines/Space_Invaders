8356bf1049f116920e7e33db40f10ac3d71a087e
guavalines <konstrictor@gmail.com>
2023-04-08 22:17:10 -0700

    coded draw function

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ for (let i = 0; i < 225; i++) {
   grid.appendChild(square)
 }
 
+const squares = Array.from(document.querySelectorAll('.grid div'))
+
 const alienInvaders = [
   0,1,2,3,4,5,6,7,8,9,
   15,16,17,18,19,20,21,22,23,24,
   30,31,32,33,34,35,36,37,38,39
 ]
+
+function draw() {
+  for (let i = 0; i < alienInvaders.length; i++) {
+    squares[alienInvaders[i]].classList.add('invader')
+  }
+}
