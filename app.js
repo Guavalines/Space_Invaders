2458b41306a32a2166e11d87dce53d569d91b8fc
guavalines <konstrictor@gmail.com>
2023-04-09 23:22:53 -0700

    added move shooter function

--- app.js
+++ app.js
@@ -23,3 +23,17 @@ function draw() {
 draw()
 
 squares[currentShooterIndex].classList.add('shooter')
+
+function moveShooter(e) {
+  squares[currentShooterIndex].classList.remove('shooter')
+  switch(e.key) {
+    case 'ArrowLeft':
+      if (currentShooterIndex % width !== 0) currentShooterIndex -=1
+      break
+    case 'ArrowRight' :
+      if (currentShooterIndex % width < width -1) currentShooterIndex +=1
+      break
+  }
+  squares[currentShooterIndex].classList.add('shooter')
+}
+document.addEventListener('keydown', moveShooter)
